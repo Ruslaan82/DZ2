be31e582649d293b070f906ed4d4c5136fe00626
Ruslaan82 <abdrakhmanovv82@gmail.com>
2023-01-30 20:22:06 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Написать программу "Треугольник в цикле".
+// Программа должна в консоль нарисовать следующую фигуру(см. скриншот)
+
+// const iterator = Array(7).keys();
+// const arr = Array.from(iterator);
+//
+// arr.reduce(acc => {
+//     const result = acc + '#'
+//     console.log(result)
+//     return result
+// }, '')
+
+const limit = 7;
+let counter = 0;
+
+while (counter <= limit) {
+    let result = '';
+    for (let i = 0; i < counter; i++) {
+        result += '*'
+    }
+    console.log(result)
+    counter++
+}
